042807637411b2ffb25f73febbc260fd99cb58f3
zhlou <zhlou@a3e5a195-83b0-4c6b-8637-e6d081586ea5>
2013-03-01 22:02:42 +0000

    git-svn-id: http://zhihaolou.org/svn/neoParSA/trunk@102 a3e5a195-83b0-4c6b-8637-e6d081586ea5

--- tests/fly/printscore.cpp
+++ tests/fly/printscore.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <libxml/parser.h>
+#include <unistd.h>
 #include "fly.h"
 
 using namespace std;
@@ -16,11 +17,23 @@ int main(int argc, char **argv)
     if (argc <= 1) {
         throw runtime_error("Missing input files");
     }
-    char *docname = argv[1];
+    string section;
+    char c;
+    while ( (c = getopt(argc, argv, "x:")) != -1 ) {
+        switch (c) {
+        case 'x':
+            section = optarg;
+            break;
+        }
+    }
+    char *docname = argv[optind];
     xmlDoc *doc = xmlParseFile(docname);
     xmlNode *root = xmlDocGetRootElement(doc);
     fly_params flyParams = readFlyParams(root);
+    if (!section.empty())
+        flyParams.section_title = section;
     fly theFly(flyParams);
+    cout.precision(flyParams.ndigits);
     cout << "chisq = " << theFly.get_score() << " rms = " << theFly.get_rms()
             << endl;
     return 0;
