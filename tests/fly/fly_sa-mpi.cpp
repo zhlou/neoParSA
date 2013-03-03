99d5b7dad05914574e4912cd9d3123e76e1bb489
zhlou <zhlou@a3e5a195-83b0-4c6b-8637-e6d081586ea5>
2013-03-03 07:02:41 +0000

    fly_sa-mpi is now on board
    git-svn-id: http://zhihaolou.org/svn/neoParSA/trunk@104 a3e5a195-83b0-4c6b-8637-e6d081586ea5

---
+++ tests/fly/fly_sa-mpi.cpp
@@ -0,0 +1,64 @@
+/*
+ * fly_sa-mpi.cpp
+ *
+ *  Created on: Mar 3, 2013
+ *      Author: zhlou
+ */
+
+#include <iostream>
+#include <libxml/parser.h>
+#include <mpi.h>
+
+#include "annealer.h"
+#include "parallelFBMove.h"
+#include "unirandom.h"
+#include "plsa.h"
+#include "debugOut.h"
+#include "adaptMix.h"
+#include "fly.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    MPIState mpi;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi.nnodes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi.rank);
+    MPI_Comm_group(MPI_COMM_WORLD, &mpi.group);
+    mpi.comm = MPI_COMM_WORLD;
+
+    if (argc <= 1) {
+        cerr << "Missing input files" << endl;
+        return 1;
+    }
+    char *docname = argv[1];
+    xmlDoc *doc = xmlParseFile(docname);
+    xmlNode *docroot = xmlDocGetRootElement(doc);
+    if (docroot == NULL) {
+        cerr << "Input incorrect" << endl;
+        return 2;
+    }
+    unirandom rnd(mpi.rank);
+    fly_params flyParams = readFlyParams(docroot);
+    fly theFly(flyParams);
+    parallelFBMove<fly, debugSTD, adaptMix> *fly_problem =
+            new parallelFBMove<fly, debugSTD, adaptMix>(theFly, rnd, docroot, mpi);
+    plsa *pschedule = new plsa(docroot, mpi);
+    annealer<plsa, parallelFBMove<fly, debugSTD, adaptMix>, unirandom>
+        fly_sa(*pschedule, *fly_problem, rnd, docroot);
+    cout << "The initial energy is " << theFly.get_score() << endl;
+    fly_sa.loop();
+    cout << "The final energy is " << theFly.get_score() << endl;
+    if (fly_problem->getWinner() == mpi.rank)
+        theFly.writeAnswer();
+    delete pschedule;
+    delete fly_problem;
+    MPI_Finalize();
+
+    return 0;
+}
+
+
+
+
