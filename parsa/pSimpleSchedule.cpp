f8ae0021239a5e8937026f8dc6b55d585665e75a
zhlou <zhlou@a3e5a195-83b0-4c6b-8637-e6d081586ea5>
2013-04-03 19:04:53 +0000

    adding more debug output
    git-svn-id: http://zhihaolou.org/svn/neoParSA/trunk@144 a3e5a195-83b0-4c6b-8637-e6d081586ea5

--- parsa/pSimpleSchedule.cpp
+++ parsa/pSimpleSchedule.cpp
@@ -31,6 +31,7 @@ bool pSimpleSchedule::frozen(aState)
         cnt = 0;
         unsigned tot_rej;
         MPI_Allreduce(&reject_cnt, &tot_rej, 1, MPI_INT, MPI_SUM, mpi.comm);
+        debugOut << tot_rej << "\t" << max_rej * mpi.nnodes << std::endl;
         return (tot_rej >= max_rej * mpi.nnodes);
     }
     return false;
