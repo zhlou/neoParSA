8b691e1635275f648e2d8bc392041c18ca267ed9
zhlou <zhlou@a3e5a195-83b0-4c6b-8637-e6d081586ea5>
2013-04-03 22:11:30 +0000

    added new rejCount class as part of separate frozen condition algorithm
    git-svn-id: http://zhihaolou.org/svn/neoParSA/trunk@145 a3e5a195-83b0-4c6b-8637-e6d081586ea5

---
+++ parsa/rejCount.cpp
@@ -0,0 +1,18 @@
+/*
+ * rejCount.cpp
+ *
+ *  Created on: Apr 3, 2013
+ *      Author: zhlou
+ */
+
+#include "rejCount.h"
+
+rejCount::rejCount() {
+    // TODO Auto-generated constructor stub
+
+}
+
+rejCount::~rejCount() {
+    // TODO Auto-generated destructor stub
+}
+
---
+++ parsa/rejCount.h
@@ -0,0 +1,17 @@
+/*
+ * rejCount.h
+ *
+ *  Created on: Apr 3, 2013
+ *      Author: zhlou
+ */
+
+#ifndef REJCOUNT_H_
+#define REJCOUNT_H_
+
+class rejCount {
+public:
+    rejCount();
+    virtual ~rejCount();
+};
+
+#endif /* REJCOUNT_H_ */
