14ec8989ebeea9a2351ba590704948b1b4d24467
zhlou <zhlou@a3e5a195-83b0-4c6b-8637-e6d081586ea5>
2013-04-04 23:18:19 +0000

    more rejCount stuff
    git-svn-id: http://zhihaolou.org/svn/neoParSA/trunk@147 a3e5a195-83b0-4c6b-8637-e6d081586ea5

--- parsa/rejCount.cpp
+++ parsa/rejCount.cpp
@@ -7,12 +7,23 @@
 
 #include "rejCount.h"
 
-rejCount::rejCount(const Param &param) {
+rejCount::rejCount(const Param &param) :
+    debugOut(param.st, param.debugname)
+{
     // TODO Auto-generated constructor stub
-
+    reject_cnt = 0;
+    max_rej = param.max_rej;
+    output_freq = param.output_freq;
 }
 
 rejCount::~rejCount() {
     // TODO Auto-generated destructor stub
 }
 
+void rejCount::updateState(bool accept, const aState &)
+{
+    if (accept)
+        reject_cnt = 0;
+    else
+        ++ reject_cnt;
+}
--- parsa/rejCount.h
+++ parsa/rejCount.h
@@ -7,13 +7,19 @@
 
 #ifndef REJCOUNT_H_
 #define REJCOUNT_H_
+
+#include <libxml/tree.h>
 #include "aState.h"
+#include "dynDebug.h"
+
 class rejCount {
 public:
     class Param {
     public:
         int max_rej;
         int output_freq;
+        debugStatus st;
+        const char * debugname;
     };
     rejCount(const Param &param);
     virtual ~rejCount();
@@ -25,5 +31,6 @@ protected:
     int output_freq;
     int reject_cnt;
 };
-
+rejCount::Param rejCountParamXML(xmlNode *root, debugStatus st=ignore,
+                                 const char * debugname=NULL);
 #endif /* REJCOUNT_H_ */
