9121a882d71a16bf5a648fb40a7b7088f3d9d554
zhlou <zhlou@a3e5a195-83b0-4c6b-8637-e6d081586ea5>
2014-05-30 03:53:53 +0000

    added tsp_lam
    git-svn-id: http://zhihaolou.org/svn/neoParSA/trunk@241 a3e5a195-83b0-4c6b-8637-e6d081586ea5

---
+++ tests/tsp/tsp_lam.cpp
@@ -0,0 +1,96 @@
+/*
+ * tsp_lam.cpp
+ *
+ *  Created on: May 29, 2014
+ *      Author: zhlou
+ */
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <unistd.h>
+#include <cstring>
+#include <libxml/parser.h>
+
+#include "unirandom.h"
+#include "criCount.h"
+#include "lam.h"
+#include "feedbackMove.h"
+#include "annealer.h"
+#include "tsp.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    bool isprolix = false;
+    bool issteplog = true;
+    bool isequil = false;
+
+    std::string section;
+    std::string binname(basename(argv[0]));
+    try {
+        char c;
+        while ( (c = getopt(argc, argv, "EpL")) != -1) {
+            switch(c) {
+            case 'E':
+                isequil = true;
+                break;
+            case 'L':
+                issteplog = false;
+                break;
+            case 'p':
+                isprolix = true;
+                break;
+            default:
+                throw std::runtime_error("Unrecognized option");
+            }
+        }
+        if (argc <= optind) {
+            throw std::runtime_error("Missing input file");
+        }
+    } catch (std::exception &ex) {
+        std::cerr << ex.what() << std::endl;
+        std::cerr << "Usage: " << binname << " [-E] [-L] [-p] input_file"
+                  << std::endl;
+        return -1;
+    }
+    char *docname = argv[optind];
+    xmlDocPtr xmldoc = xmlParseFile(docname);
+    xmlNodePtr xmlroot = xmlDocGetRootElement(xmldoc);
+
+    unirand48 rnd;
+    tsp theTSP(xmlroot);
+    lam::Param scheduleParam(xmlroot);
+    criCount::Param frozenParam(xmlroot);
+    annealer<tsp, lam, criCount, feedbackMove>
+        tsp_sa(theTSP, rnd, scheduleParam, frozenParam, xmlroot);
+    string basename(docname);
+    size_t sz = basename.size();
+    basename.resize(sz-4);
+    string outprefix = basename;
+    if (isprolix) {
+        tsp_sa.setProlix(file, (outprefix + ".prolix").c_str());
+    }
+
+    tsp_sa.setCoolLog(file,(basename + ".log").c_str());
+
+    if (issteplog) {
+        tsp_sa.setStepLog(file, (outprefix + ".steplog").c_str());
+    }
+
+    if (isequil) {
+        tsp_sa.initMovesOnly();
+    } else {
+        cout << "The initial energy is " << theTSP.get_score() << endl;
+        tsp_sa.loop();
+        cout << "Final energy is " << theTSP.get_score() << endl;
+        cout << "Actual energy is " << theTSP.calc_tour() << endl;
+        theTSP.write_tour(xmlroot);
+        tsp_sa.writeResult();
+        xmlSaveFormatFile(docname, xmldoc,1);
+    }
+    xmlFreeDoc(xmldoc);
+    xmlCleanupParser();
+    return 0;
+}
