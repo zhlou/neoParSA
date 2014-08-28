8dcfa4f25c29cfd68eade3b9d6e83ddac6aaf6ad
zhlou <zhlou@a3e5a195-83b0-4c6b-8637-e6d081586ea5>
2014-08-28 17:25:06 +0000

    Added feedbackMix that trying to keep the rate of adopted states constant
    git-svn-id: http://zhihaolou.org/svn/neoParSA/trunk@263 a3e5a195-83b0-4c6b-8637-e6d081586ea5

---
+++ parsa/feedbackMix.h
@@ -0,0 +1,43 @@
+/*
+ * feedbackMix.h
+ *
+ *  Created on: Aug 27, 2014
+ *      Author: zhlou
+ */
+
+#ifndef FEEDBACKMIX_H_
+#define FEEDBACKMIX_H_
+
+#include <libxml/tree.h>
+#include "dynDebug.h"
+#include "MPIState.h"
+#include "mixState.h"
+#include "Mixing.h"
+#include "aState.h"
+
+template <class Problem>
+class feedbackMix {
+public:
+    feedbackMix(Problem &problem, const MPIState &mpiState,
+                unirandom& rand, xmlNode *docroot);
+    ~feedbackMix();
+    mixState Mix(aState &state);
+    static const char * name;
+    void setDebug(debugStatus st, const char* outname=NULL)
+    {
+        debugOut.setDebug(st, outname);
+    }
+private:
+    Mixing<Problem> mix;
+    xmlNode *root;
+    double target;
+    int interval;
+    int tau_count;
+    const MPIState &mpi;
+    int *adoptArray;
+    dynDebug debugOut;
+};
+
+#include "feedbackMix.hpp"
+
+#endif /* FEEDBACKMIX_H_ */
---
+++ parsa/feedbackMix.hpp
@@ -0,0 +1,65 @@
+/*
+ * feedbackMix.hpp
+ *
+ *  Created on: Aug 27, 2014
+ *      Author: zhlou
+ */
+
+#ifndef FEEDBACKMIX_HPP_
+#define FEEDBACKMIX_HPP_
+
+#include <cmath>
+#include <mpi.h>
+#include "utils.h"
+
+template<class Problem>
+const char * feedbackMix<Problem>::name = "feedbackMix";
+
+template<class Problem>
+feedbackMix<Problem>::feedbackMix(Problem &problem, const MPIState &mpiState,
+                                  unirandom &rand, xmlNode *docroot):
+        mix(problem, mpiState, rand), root(docroot),tau_count(0),
+        mpi(mpiState), target(0.5)
+{
+    xmlNode *section = getSectionByName(root, "feedbackMix");
+    interval = getPropInt(section, "interval");
+    adoptArray = new int[mpi.nnodes];
+
+}
+
+template<class Problem>
+feedbackMix<Problem>::~feedbackMix()
+{
+    delete []adoptArray;
+}
+
+template<class Problem>
+mixState feedbackMix<Problem>::Mix(aState &state)
+{
+    ++tau_count;
+    if ((tau_count % interval) != 0)
+        return mixState();
+    int i,p, nadopt=0;
+    const double C = 1.38629436112; // 2ln(2)
+    tau_count = 0;
+    mix.calProbTab(state);
+    p = mix.getPartner();
+    state.energy = mix.adoptState(p);
+    for (i = 0; i < mpi.nnodes; ++ i)
+        adoptArray[i] = 0;
+    adoptArray[p] = 1;
+    MPI_Allreduce(MPI_IN_PLACE, adoptArray, mpi.nnodes, MPI_INT, MPI_LOR,
+                  mpi.comm);
+    for (i = 0; i < mpi.nnodes; ++i)
+        nadopt+=adoptArray[i];
+    double adoptRate = (double)(nadopt) / mpi.nnodes;
+    interval *= std::exp(C*(adoptRate - target));
+    if (interval < 1)
+        interval = 1;
+    debugOut << state.step_cnt << " " << state.s << " "
+             << adoptRate << " " << interval << std::endl;
+    return mixState(p);
+
+}
+
+#endif /* FEEDBACKMIX_HPP_ */
