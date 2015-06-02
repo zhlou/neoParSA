147d08d5dab1489b086e92c979620aa31dd1c7fe
zhlou <zhlou@a3e5a195-83b0-4c6b-8637-e6d081586ea5>
2015-06-02 22:14:22 +0000

    backported transcpp_expHold
    git-svn-id: http://zhihaolou.org/svn/neoParSA/trunk@340 a3e5a195-83b0-4c6b-8637-e6d081586ea5

--- tests/transcpp/transcpp_expHold.cpp
+++ tests/transcpp/transcpp_expHold.cpp
@@ -4,20 +4,22 @@
  *
  * Created on November 18, 2014, 11:05 AM
  */
-
-#include "flags.h"
 #include "pwm.h"
 #include "TF.h"
 #include "gene.h"
-#include "conc.h"
+#include "datatable.h"
 #include "twobit.h"
 #include "organism.h"
+#include "mode.h"
+#include "utils.h"
 #include <fstream>
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
+#include <boost/lexical_cast.hpp>
 
 #include <unistd.h>
 #include <libxml/parser.h>
+#include <getopt.h>
 #include "annealer.h"
 #include "move/feedbackMove.h"
 #include "unirandom.h"
@@ -39,11 +41,38 @@ int main(int argc, char** argv)
 {
     bool isprolix = false;
     bool issteplog = false;
+    int iscoollog = 0;
+    int saveInitState = 0;
+    int readInitState = 0;
+    int optIndex;
+    char *saveStatePrefix = NULL;
+    char *readStatePrefix = NULL;
+
+    struct option long_options[] = {
+        {"save-state", 1, &saveInitState, 1},
+        {"read-state", 1, &readInitState, 1},
+        {"cool-log", 0, &iscoollog, 1},
+        {0, 0, 0, 0}
+    };
     std::string binname(basename(argv[0]));
     try {
         char c;
-        while ((c = getopt(argc, argv, "pl")) != -1) {
+        while ((c = getopt_long(argc, argv, "pl", long_options, &optIndex)) != -1) {
             switch (c) {
+            case 0:
+                switch (optIndex) {
+                case 0:
+                    saveStatePrefix = optarg;
+                    break;
+                case 1:
+                    readStatePrefix = optarg;
+                    break;
+                case 2:
+                    break;
+                default:
+                    throw std::runtime_error("Unrecognized option");
+                }
+                break;
             case 'l':
                 issteplog = true;
                 break;
@@ -69,13 +98,22 @@ int main(int argc, char** argv)
 
     ptree pt;
     read_xml(infile, pt, boost::property_tree::xml_parser::trim_whitespace);
+    infile.close();
+    
+    ptree& root_node = pt.get_child("Root");
+    ptree& mode_node = root_node.get_child("Mode");
+    ptree& input_node = root_node.get_child("Input");
 
-    ptree & input = pt.get_child("Input");
+    mode_ptr mode(new Mode(xmlname, mode_node));
 
-    Organism embryo(input);
+    Organism embryo(input_node, mode);
     //embryo.printParameters(cerr);
 
     unirand48 rnd;
+    unsigned int seed = mode->getSeed();
+    if (mode->getVerbose() >= 1) 
+        cerr << "Beginning annealing with seed " << seed << endl;
+    rnd.setSeed(seed);
     //rnd.setSeed(getpid());
 
     xmlDoc *doc = xmlParseFile(xmlname.c_str());
@@ -84,28 +122,45 @@ int main(int argc, char** argv)
     tempCount::Param frozenParam(docroot);
     annealer<Organism, expHold, tempCount, feedbackMove>
             annealer(embryo, rnd, scheParam, frozenParam, docroot);
+    
+    string basename(xmlname);
+    size_t sz = basename.size();
+    basename.resize(sz-4);
+    
     if (isprolix)
-        annealer.setProlix(file, (xmlname + ".prolix").c_str());
-    annealer.setCoolLog(file, (xmlname + ".log").c_str());
+        annealer.setProlix(file, (basename + ".prolix").c_str());
+    if (iscoollog)
+        annealer.setCoolLog(file, (basename + ".log").c_str());
     if (issteplog)
-        annealer.setStepLog(file, (xmlname+".steplog").c_str());
+        annealer.setStepLog(file, (basename+".steplog").c_str());
+
+    if (saveInitState) {
+        annealer.initMoves();
+        cerr << "The energy after initMoves is " << embryo.get_score() << endl;
+        //embryo.printScore(cout);
+        annealer.saveUnifiedInitState(saveStatePrefix);
+    } else {
+        if (readInitState) {
+            annealer.readUnifiedInitState(readStatePrefix);
+        }
+
+        cerr << "The energy is " << embryo.get_score() << endl;
+        annealer.loop();
+        cerr << "The energy is " << embryo.get_score() << " after loop" << endl;
 
 
+        //embryo.printParameters(cerr);
 
-    cerr << "The energy is " << embryo.get_score() << endl;
-    annealer.loop();
-    cerr << "The energy is " << embryo.get_score() << " after loop" << endl;
+        //ptree output, anneal_output;
+        embryo.write("Output", root_node);
+        annealer.ptreeGetResult(root_node);
+        //ptree& opt = output.get_child("Output");
+        //output.put_child("Output.anneal_output", anneal_output);
+        boost::property_tree::xml_writer_settings<char> settings(' ', 2);
+        write_xml(xmlname, pt, std::locale(), settings);
+    }
     xmlFreeDoc(doc);
-
-    //embryo.printParameters(cerr);
-
-    ptree output, anneal_output;
-    embryo.write("Output", output);
-    annealer.ptreeGetResult(output.get_child("Output"));
-    //ptree& opt = output.get_child("Output");
-    output.put_child("Output.anneal_output", anneal_output);
-    boost::property_tree::xml_writer_settings<char> settings(' ', 2);
-    write_xml_element(infile, basic_string<ptree::key_type::value_type>(), output, -1, settings);
+    xmlCleanupParser();
 
     return 0;
 }
