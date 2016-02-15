def891ca014548f00fcc01385700dda6a23d3614
Zhihao Lou <lzh1984@gmail.com>
2016-02-14 20:18:44 -0600

    new transcpp_pEF-st-mix using scheduleTheta in parallel

--- parsa/move/scheduleThetaMoveP.h
+++ parsa/move/scheduleThetaMoveP.h
@@ -4,12 +4,15 @@
 
 #include "scheduleThetaMove.h"
 #include "MPIState.h"
+#include "mix/mixState.h"
 
 template <class Problem>
 class scheduleThetaMoveP : public scheduleThetaMove<Problem>
 {
 public:
-    scheduleThetaMoveP(Problem &in_problem, unirandom &in_rnd, const ptree &root, MPIState &mpi) : scheduleThetaMove<Problem>(in_problem, in_rnd, root) {}
+    scheduleThetaMoveP(Problem &in_problem, unirandom &in_rnd, const ptree &root, const MPIState &mpi) : scheduleThetaMove<Problem>(in_problem, in_rnd, root) {}
+    void processMix(const mixState &ms, const aState &state)
+    {if (ms.doesMix()) this->energy = state.energy;}
 };
 
 
