a82c564ecbe8221e68f4c6f61328b935c18ecc88
Zhihao Lou <lzh1984@gmail.com>
2016-03-21 17:22:28 -0500

    finished implementing tempFeedback

--- parsa/move/tempFeedback.h
+++ parsa/move/tempFeedback.h
@@ -15,24 +15,27 @@ class tempFeedback
 {
 public:
     tempFeedback(Problem &in_problem, unirandom &in_rnd, const ptree &root);
-    virtual ~tempFeedback();
-    double get_score();
+    // now every array become a vector, I don't need to do anything in dtor
+    virtual ~tempFeedback() {}
+    double get_score() {return energy;}
     double propose(const aState &);
     void accept();
     void reject();
     static const char *name;
-    void setDebug(debugState st, const char*outname=NULL)
+    void setDebug(debugStatus st, const char*outname=NULL)
     {debugOut.setDebug(st, outname);}
     void writeState(ptree &root) const;
     void readState(const ptree &root);
     double forceUpdateEnergy(){return energy = problem.get_score();}
 protected:
-    virtual void collectMoveStats(){};
+    virtual void collectMoveStats(){}; // do nothing in base class
     void move_control();
     size_t nparams;
     std::vector<unsigned long> success;
     std::vector<unsigned long> moves;
     std::vector<double> theta_bars;
+    std::vector<double> temp_coef;
+    std::vector<double> last_s;
     std::vector<double> theta_mins;
     std::vector<double> theta_maxs;
 private:
@@ -41,13 +44,15 @@ private:
     dynDebug debugOut;
     double energy;
     double prev_energy;
+    double prev_s;
     size_t index;
     double proportion_gain; // should be const
     double integral_gain; // should be const
     unsigned long sweep;
     unsigned long interval; // should be const
     double target; // should be const
-}
+    double genThetaBar(size_t i, double s);
+};
 
 #include "tempFeedback.hpp"
 #endif
--- parsa/move/tempFeedback.hpp
+++ parsa/move/tempFeedback.hpp
@@ -4,7 +4,7 @@
 #include <limits>
 
 template<class Problem>
-const char *tempFeedback::name = "tempFeedback";
+const char *tempFeedback<Problem>::name = "tempFeedback";
 
 template<class Problem>
 tempFeedback<Problem>::tempFeedback(Problem &in_problem, unirandom &in_rnd,
@@ -13,6 +13,7 @@ tempFeedback<Problem>::tempFeedback(Problem &in_problem, unirandom &in_rnd,
         success(nparams, 0),
         moves(nparams, 0),
         theta_bars(nparams, 1.0),
+        temp_coef(nparams, 0.0),
         theta_mins(nparams, 0.),
         theta_maxs(nparams, std::numeric_limits<double>::max()),
         problem(in_problem),
@@ -29,7 +30,7 @@ tempFeedback<Problem>::tempFeedback(Problem &in_problem, unirandom &in_rnd,
             theta_bars[i] = *initTheta;
         }
     }
-    std::pair <tree::const_assoc_iterator, ptree::const_assoc_iterator> bonds
+    std::pair <ptree::const_assoc_iterator, ptree::const_assoc_iterator> bounds
             = root.get_child("tempMove").equal_range("theta");
     for (ptree::const_assoc_iterator it = bounds.first; it != bounds.second; ++it) {
         const ptree &node = it->second;
@@ -39,6 +40,112 @@ tempFeedback<Problem>::tempFeedback(Problem &in_problem, unirandom &in_rnd,
         theta_bars[i] = node.get<double>("<xmalattr>.val", theta_bars[i]);
         theta_mins[i] = node.get<double>("<xmlattr>.min", theta_mins[i]);
         theta_maxs[i] = node.get<double>("<xmlattr>.max", theta_maxs[i]);
+    }
+}
+
+template <class Problem>
+double tempFeedback<Problem>::propose(const aState &state)
+{
+    index ++;
+    if (index == nparams) {
+        index = 0;
+        ++sweep;
+        if (sweep == 0) {
+            sweep = 0;
+            collectMoveStats();
+            move_control();
+        }
+    }
+    prev_energy = energy;
+    double theta = rnd.laplace(genThetaBar(index, state.s));
+    problem.generateMove(index, theta);
+    ++ moves[index];
+    energy = problem.get_score();
+    return (energy - prev_energy);
+}
+
+template<class Problem>
+void tempFeedback<Problem>::accept()
+{
+    ++ success[index];
+}
+
+template<class Problem>
+void tempFeedback<Problem>::reject()
+{
+    problem.restoreMove(index);
+    energy = prev_energy;
+}
+
+template<class Problem>
+void tempFeedback<Problem>::move_control()
+{
+    debugOut << sweep;
+    for (int i = 0; i < nparams; ++i) {
+        double acc_ratio = (double)success[i] / (double) moves[i];
+        double delta_rho = acc_ratio - target;
+        temp_coef[i] += integral_gain * delta_rho;
+        double lt = std::log(theta_bars[i]);
+        lt += proportion_gain * delta_rho;
+        double t = std::exp(lt);
+        if (t < theta_mins[i]) {
+            t = theta_mins[i];
+        }
+        if (t > theta_maxs[i]) {
+            t = theta_maxs[i];
+        }
+        theta_bars[i] = t;
+    }
+}
+
+template<class Problem>
+double tempFeedback<Problem>::genThetaBar(size_t i, double s)
+{
+    if (0 != last_s[i]) {
+        double lt = std::log(theta_bars[i]);
+        lt += temp_coef[i] * (std::log(s) - std::log(last_s[i]));
+        double t = std::exp(lt);
+        if (t < theta_mins[i]) {
+            t = theta_mins[i];
+        }
+        if (t > theta_maxs[i]) {
+            t = theta_maxs[i];
+        }
+        theta_bars[i] = t;
+    }
+    last_s[i] = s;
+    return theta_bars[i];
+}
+
+template<class Problem>
+void tempFeedback<Problem>::writeState(ptree &root) const
+{
+    ptree node;
+    for (int i = 0; i < nparams; ++i) {
+        ptree &param = node.put("param", i);
+        param.put("<xmlattr>.theta", theta_bars[i]);
+        param.put("<xmlattr>.temp_coef", temp_coef[i]);
+    }
+    root.put_child("moveSize", node);
+}
+template<class Problem>
+void tempFeedback<Problem>::readState(const ptree &root)
+{
+    boost::optional<const ptree &> section = root.get_child_optional("moveSize");
+    if (! section) {
+        std::cerr << "Warning: fail to read moveSize from state file\n";
+        return;
+    }
+    std::pair <ptree::const_assoc_iterator, ptree::const_assoc_iterator>
+            bounds = section->equal_range("param");
+    for (ptree::const_assoc_iterator it = bounds.first;
+            it != bounds.second; ++it) {
+        const ptree &node = it->second;
+        int i = node.get_value<int>(0);
+        if (i < 0 || i >= nparams)
+            continue;
+        theta_bars[i] = node.get<double>("<xmlattr>.theta", theta_bars[i]);
+        temp_coef[i] = node.get<double>("<xmlattr>.temp_coef", temp_coef[i]);
     }
 }
 
